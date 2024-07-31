7e802aa43c467dc21b556c0289a676a989b41b8d
Syed <syhussaini@users.noreply.github.com>
2024-07-31 18:39:01 +0530

    Update WordPress-Site-Detector.js
    Updated update/Download URLs

--- wordpress-site-detector/WordPress-Site-Detector.js
+++ wordpress-site-detector/WordPress-Site-Detector.js
@@ -6,8 +6,8 @@
 // @author       Syed Hussaini - Email: hussaini.syed@gmail.com
 // @match        *://*/*
 // @grant        GM_addStyle
-// @updateURL    https://raw.githubusercontent.com/syhussaini/TamperMonkey_scripts/main/wordpress-site-detector/WordPress-Site-Detector.js
-// @downloadURL  https://raw.githubusercontent.com/syhussaini/TamperMonkey_scripts/main/wordpress-site-detector/WordPress-Site-Detector.js
+// @updateURL    https://githubusercontent.com/syhussaini/TamperMonkey_scripts/main/wordpress-site-detector/WordPress-Site-Detector.js
+// @downloadURL  https://githubusercontent.com/syhussaini/TamperMonkey_scripts/main/wordpress-site-detector/WordPress-Site-Detector.js
 // ==/UserScript==
 
 (function() {
